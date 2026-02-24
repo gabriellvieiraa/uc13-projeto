047f41732f1129dc47c4c28822700f5ad15248c6
Nik4o6 <nicolascoelho2011@hotmail.com>
2026-02-24 20:01:35 -0300

    POST

---
+++ src/routes/companie.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+import { createCompanie } from "../services/companie";
+
+
+const router = Router();
+
+router.post('/create',createCompanie);
---
+++ src/services/companie.js
@@ -0,0 +1,18 @@
+import { PrismaCliet } from "@prisma/client"
+import { z } from 'zod'
+const prisma = PrismaCliet();
+
+//req: requisição o que esta vindo do frontend
+//req: responde o que eu vou responder
+//next: proximo o que vou fazer seguir
+
+export async function createCompanie(req, res, _next){
+    const data = req.body;
+    let u =  await prisma.companie.create({data});
+    return res.status(201).json(u);
+  
+
+
+
+
+}
