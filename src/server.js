72572c11d18a1703f387ac50ef53ca3a576348d9
gabriellvieiraa <gabrieldicassiovieira@gmail.com>
2026-02-24 20:46:55 -0300

    gabriel server

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ import express from 'express'; //framework
 import cors from 'cors'; //ele vai permitir a comunicação com frontend
 import companieRouter from './routes/companie'
 import routerCourse from './routes/course'
+import routerCategorie from './routes/categorie'
 
 const app = express() //estou criando o app
 app.use(cors()); //aqui eu falo qual cors
 app.use(express.json()); //aqui eu falo o formato do json
 
 app.use('/companie',companieRouter); 
-app.use('/course',routerCourse);   
+app.use('/course',routerCourse); 
+app.use('/categorie', routerCategorie);  
                                        //USER,COMPANIE,ETC
 
 
