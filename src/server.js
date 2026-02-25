5dc7d40a3b5dfb9186e36031d5f2ce204979d5f3
vanssales <vanssales@gmail.com>
2026-02-24 21:30:44 -0300

    vanessa server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from 'cors'; //ele vai permitir a comunicação com frontend
 import companieRouter from './routes/companie'
 import routerCourse from './routes/course'
 import routerCategorie from './routes/categorie'
+import routerUser from './routes/user'
 
 const app = express() //estou criando o app
 app.use(cors()); //aqui eu falo qual cors
@@ -12,6 +13,7 @@ app.use(express.json()); //aqui eu falo o formato do json
 app.use('/companie',companieRouter); 
 app.use('/course',routerCourse); 
 app.use('/categorie', routerCategorie);  
+app.use('/user', routerUser);  
                                        //USER,COMPANIE,ETC
 
 
