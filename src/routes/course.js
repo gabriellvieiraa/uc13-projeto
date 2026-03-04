7b109ae28a24c0c2ca7a116894fe55861a526664
leonerescoder <leonardoneres93@gmail.com>
2026-03-03 21:17:58 -0300

    search courses

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
-import { createCourse, readCourse} from '../services/course.js';
+import { createCourse, readCourse , showCourse} from '../services/course.js';
 
 const router= Router();
 
 router.post('/' , createCourse); 
 router.get('/', readCourse)
+router.get('/:id', showCourse)
 
 
 export default router;
