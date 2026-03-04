231b75869e5ac9363a2eef00d40926b529cceb5b
Nik4o6 <nicolascoelho2011@hotmail.com>
2026-03-03 22:14:07 -0300

    QUERY CORRECTION

--- src/routes/companie.js
+++ src/routes/companie.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 
-import { createCompanie, readCompanie } from '../services/companie.js';
+import { createCompanie, readCompanie, showCompanie} from '../services/companie.js';
 
 
 const router = Router();
 
 router.post('/', createCompanie); //create e post são a mesma coisa
 router.get('/', readCompanie);
+router.get('/:id', showCompanie);
 
 export default router;
