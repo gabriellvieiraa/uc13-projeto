e55618d7b2e6875f1681dd08f600634c9fa2af19
leonerescoder <leonardoneres93@gmail.com>
2026-03-10 21:26:46 -0300

    leo

--- src/services/course.js
+++ src/services/course.js
@@ -1,6 +1,8 @@
 import {PrismaClient} from "@prisma/client";
 import { z } from 'zod';
 
+import { attachSave } from "./save.js";
+
 const prisma = new PrismaClient();
 
 //req: requisição o que está vindo do Frontend
