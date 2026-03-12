9caa887ad2d2c0a133d3b6527dfa67fb79e0a0f1
leonerescoder <leonardoneres93@gmail.com>
2026-03-12 20:26:38 -0300

    delete

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { createCourse, readCourse , showCourse , editCourse} from '../services/course.js';
+import { createCourse, readCourse , showCourse , editCourse,deleteCourse} from '../services/course.js';
 
 const router= Router();
 
@@ -7,6 +7,7 @@ router.post('/' , createCourse);
 router.get('/', readCourse)
 router.get('/:id', showCourse)
 router.put('/:id', editCourse)
+router.delete('/:id', deleteCourse)
 
 
 export default router;
